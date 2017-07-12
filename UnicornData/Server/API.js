fabcb035a5091ccb2e02b90864010630212bd6ff
Ross Stokoe <rossmjs@gmail.com>
2017-07-12 20:12:07 +0100

    Added local mysql db to test query.
    Defined API for muff to add content of varable "UserInput" from user (has to be ferrari though)

---
+++ UnicornData/Server/API.js
@@ -0,0 +1,26 @@
+﻿
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "unicorndata"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+var UserInput = require('USERINPUT'); 
+
+con.connect(function (err) {
+    if (err) throw err;
+
+    con.query("SELECT * FROM mydb.cars WHERE MATCH(make) AGAINST('USERINPUT' IN NATURAL LANGUAGE MODE)",
+
+        function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+        });
+});
