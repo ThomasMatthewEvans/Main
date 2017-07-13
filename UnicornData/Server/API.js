4636c7e0ac97fd87dcb397e8bb268cf1b782a5a2
Ross Stokoe <rossmjs@gmail.com>
2017-07-13 20:10:59 +0100

    changed query to add union and additional tables

--- UnicornData/Server/API.js
+++ UnicornData/Server/API.js
@@ -10,17 +10,42 @@ var con = mysql.createConnection({
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
-});
 
-var UserInput = require('USERINPUT'); 
+    con.query(`SELECT * FROM (
 
-con.connect(function (err) {
-    if (err) throw err;
+            SELECT 
+            'Cars' as 'Results', Make, Model, Year, Estimate,
+            (MATCH(Make) AGAINST ('Ferrari')) as relevance 
+            from mydb.cars 
+            UNION 
+            SELECT 
+            'Manuscripts' as 'Results', Name, Author, Published_date, Estimate,
+            (MATCH(author) AGAINST ('Ferrari')) as relevance
+            from mydb.manuscripts 
+            UNION 
+            SELECT 
+            'Paintings' as 'Results', Artist, name, Date, Estimate, 
+            (MATCH(artist) AGAINST ('Ferrari')) as relevance 
+            from mydb.paintings
+)
+            as sitewide WHERE relevance > 0;`)
 
-    con.query("SELECT * FROM mydb.cars WHERE MATCH(make) AGAINST('USERINPUT' IN NATURAL LANGUAGE MODE)",
+}); 
 
-        function (err, result, fields) {
-            if (err) throw err;
-            console.log(result);
-        });
-});
+
+
+
+
+
+//var UserInput = require('USERINPUT'); 
+
+
+
+   // con.query("SELECT * FROM mydb.cars WHERE MATCH(make,model) AGAINST ('USERINPUT' IN NATURAL LANGUAGE MODE)",
+//
+//function (err, result, fields) {
+//    if (err) throw err;
+//    console.log(result);
+//});
+
+
